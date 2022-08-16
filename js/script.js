050ac6d959eb068794b0b90d0768f4d87c20e1f9
Szymon Kluka <szymonkluka@onet.pl>
2022-08-16 18:39:49 +0200

    losowanie i logika warunkowa

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
-let playerMove = `papier`;
+let randomNumber = Math.floor(Math.random() * 3 + 1);
 
-printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to kamień, to przegrywasz !');
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+
+
+if(randomNumber == 1){
+  computerMove = 'kamień';
+}
+
+else if (randomNumber == 2){
+  computerMove = 'nożyce';
+}
+
+else if
+(randomNumber == 3){
+  computerMove = 'papier';
+}  
+
+
+printMessage('Mój ruch to: ' + computerMove);
